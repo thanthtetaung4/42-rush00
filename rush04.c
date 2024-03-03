274e61196f571cd0c97db6d6fd43a31d0595865d
Thant Htet Aung <maung.good@gmail.com>
2024-03-03 16:56:11 +0800

    Rush04 lines number fixed

--- rush04.c
+++ rush04.c
@@ -6,7 +6,7 @@
 /*   By: taung <taung@student.42singapore.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 15:06:41 by taung             #+#    #+#             */
-/*   Updated: 2024/03/03 16:15:42 by taung            ###   ########.fr       */
+/*   Updated: 2024/03/03 16:55:07 by taung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,68 +17,68 @@ void	rush04(int x, int y);
 void	write_first_row(int *j_ptr, int *x_ptr);
 void	write_mid_rows(int *j_ptr, int *x_pt);
 void	write_last_row(int *j_ptr, int *x_pt);
+void	write(int i, int j, int x);
 
 void	rush(int x, int y)
 {
 	int	i;
 	int	j;
-	int	*i_ptr;
-	int	*j_ptr;
-	int	*x_ptr;
+	
 
-	i_ptr = &i;
-	j_ptr = &j;
-	x_ptr = &x;
 	i = 0;
 	while (i < y)
 	{
 		j = 0;
 		while (j < x)
 		{
-			if (i == 0)
-			{
-				write_first_row(j_ptr,x_ptr);
-			}
-			else if (i > 0 && i < y-1)
-			{
-				write_mid_rows(j_ptr,x_ptr);
-			}
-			else
-			{
-				write_last_row(j_ptr,x_ptr);
-			}
-			j++;
+			write (i, j, x);
 		}
 		ft_putchar('\n');
 		i++;
 	}
 }
 
-void	write_first_row(int *j_ptr, int *x_ptr)
+void	write_first_row(int j, int x)
 {
-	if (*j_ptr == 0)
+	if (j == 0)
 		ft_putchar('A');
-	else if (*j_ptr == *x_ptr - 1)
+	else if (j == x - 1)
 		ft_putchar('C');
 	else
 		ft_putchar('B');
 }
 
-void	write_mid_rows(int *j_ptr, int *x_ptr)
+void	write_mid_rows(int j, int x)
 {
-	if ((*j_ptr == 0) || (*j_ptr == *x_ptr - 1))
+	if ((j == 0) || (j == x - 1))
 		ft_putchar('B');
 	else
 		ft_putchar(' ');
 
 }
 
-void	write_last_row(int *j_ptr, int *x_ptr)
+void	write_last_row(int j, int x)
 {
-	if (*j_ptr == 0)
+	if (j == 0)
 		ft_putchar('C');
-	else if (*j_ptr == *x_ptr - 1)
+	else if (j == x - 1)
 		ft_putchar('A');
 	else
 		ft_putchar('B');
+}
+
+void	write(int i, int j, int x)
+{
+	if (i == 0)
+	{
+		write_first_row(j,x);
+	}
+	else if (i > 0 && i < y-1)
+	{
+		write_mid_rows(j,x);
+	}
+	else
+	{	
+		write_last_row(j,x);
+	}
 }
